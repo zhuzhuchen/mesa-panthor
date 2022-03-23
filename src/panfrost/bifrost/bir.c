c31429cda4f958d8e875cc8455f0460367f2fb09
Alyssa Rosenzweig <alyssa@collabora.com>
2022-03-23 12:05:10 -0400

    pan/bi: Update bi_count_write_registers for Valhall
    We add some new instructions on Valhall with special register requirements
    (texturing, atomics). Handle these appropriately so we can do RA on Valhall.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/panfrost/bifrost/bir.c
+++ src/panfrost/bifrost/bir.c
@@ -101,12 +101,26 @@ unsigned
 bi_count_write_registers(const bi_instr *ins, unsigned d)
 {
         if (d == 0 && bi_opcode_props[ins->op].sr_write) {
-                /* TODO: this special case is even more special, TEXC has a
-                 * generic write mask stuffed in the desc... */
-                if (ins->op == BI_OPCODE_TEXC)
+                switch (ins->op) {
+                case BI_OPCODE_TEXC:
+                        /* TODO: write mask */
                         return 4;
-                else
+
+                case BI_OPCODE_TEX_SINGLE:
+                case BI_OPCODE_TEX_FETCH:
+                case BI_OPCODE_TEX_GATHER: {
+                        unsigned chans = util_bitcount(ins->write_mask);
+
+                        return bi_is_regfmt_16(ins->register_format) ?
+                                DIV_ROUND_UP(chans, 2) : chans;
+                }
+
+                case BI_OPCODE_ATOM1_RETURN_I32:
+                        /* Allow omitting the destination for plain ATOM1 */
+                        return bi_is_null(ins->dest[0]) ? 0 : 1;
+                default:
                         return bi_count_staging_registers(ins);
+                }
         } else if (ins->op == BI_OPCODE_SEG_ADD_I64) {
                 return 2;
         } else if (ins->op == BI_OPCODE_TEXC && d == 1) {
