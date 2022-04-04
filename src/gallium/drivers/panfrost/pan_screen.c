20d681a63198bd2e3b8621c6114bf857b9b3c4d6
Alyssa Rosenzweig <alyssa@collabora.com>
2022-04-04 15:58:10 -0400

    panfrost: Don't allow vertex shaders to have side effects
    In both GL and VK, the driver may choose not to support vertex shaders with side
    effects (SSBOs, atomics, images). Supporting this opens a can of worms for IDVS.
    Neither freedreno nor the (Vulkan?) DDK advertise support, for this reason.
    Apps should not be using this anti-feature anyway.
    
    Stop advertising support.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/gallium/drivers/panfrost/pan_screen.c
+++ src/gallium/drivers/panfrost/pan_screen.c
@@ -355,6 +355,12 @@ panfrost_get_shader_param(struct pipe_screen *screen,
                 return 0;
         }
 
+        /* We only allow observable side effects (memory writes) in compute and
+         * fragment shaders. Side effects in the geometry pipeline cause
+         * trouble with IDVS.
+         */
+        bool allow_side_effects = (shader != PIPE_SHADER_VERTEX);
+
         switch (param) {
         case PIPE_SHADER_CAP_MAX_INSTRUCTIONS:
         case PIPE_SHADER_CAP_MAX_ALU_INSTRUCTIONS:
@@ -443,10 +449,10 @@ panfrost_get_shader_param(struct pipe_screen *screen,
                 return (1 << PIPE_SHADER_IR_NIR) | (1 << PIPE_SHADER_IR_NIR_SERIALIZED);
 
         case PIPE_SHADER_CAP_MAX_SHADER_BUFFERS:
-                return 16;
+                return allow_side_effects ? 16 : 0;
 
         case PIPE_SHADER_CAP_MAX_SHADER_IMAGES:
-                return PIPE_MAX_SHADER_IMAGES;
+                return allow_side_effects ? PIPE_MAX_SHADER_IMAGES : 0;
 
         case PIPE_SHADER_CAP_MAX_UNROLL_ITERATIONS_HINT:
         case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTERS:
