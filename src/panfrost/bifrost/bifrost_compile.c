1da90f1e996ce943819469ff1c6ce0c3bc69eb2d
Alyssa Rosenzweig <alyssa@collabora.com>
2022-05-02 12:17:42 -0400

    pan/bi: Restore nir_intrinsic_load_vertex_id_zero_base support
    For XFB shaders.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/panfrost/bifrost/bifrost_compile.c
+++ src/panfrost/bifrost/bifrost_compile.c
@@ -1587,6 +1587,11 @@ bi_emit_intrinsic(bi_builder *b, nir_intrinsic_instr *instr)
 
                 break;
 
+        case nir_intrinsic_load_vertex_id_zero_base:
+//                assert(!b->shader->malloc_idvs);
+                bi_mov_i32_to(b, dst, bi_vertex_id(b));
+                break;
+
         case nir_intrinsic_load_instance_id:
                 bi_mov_i32_to(b, dst, bi_instance_id(b));
                 break;
